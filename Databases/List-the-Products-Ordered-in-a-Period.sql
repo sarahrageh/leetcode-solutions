947727359e9db6f3602ca33ca2efdd54f6cb22b5
sarahrageh <sarargh2002@gmail.com>
2025-08-14 14:23:53 +0300

    [1327] [Time Beats: 40.45%] - LeetPush

---
+++ Databases/List-the-Products-Ordered-in-a-Period.sql
@@ -0,0 +1,1 @@
+select product_name, sum(unit) as unit from orders left join products using (product_id) where month(order_date) = 2 and year(order_date) = 2020 group by product_name having sum(unit) >= 100;
