7c6a0a669a9b181fa813b4faba2d487a97677ace
sarahrageh <sarargh2002@gmail.com>
2025-08-14 14:13:30 +0300

    [1280] [Time Beats: 66.27%] - LeetPush

---
+++ Databases/Students-and-Examinations.sql
@@ -0,0 +1,6 @@
+select 
+    t1.student_id, student_name, t2.subject_name, count(t3.student_id) as attended_exams
+    from students as t1 cross join subjects as t2 left join examinations as t3 
+    on t1.student_id = t3.student_id and t2.subject_name = t3.subject_name 
+    group by t1.student_id, t2.subject_name
+    order by t1.student_id, t2.subject_name;
