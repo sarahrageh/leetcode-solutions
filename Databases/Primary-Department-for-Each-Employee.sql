51bf515e912d6340237824b603c2bb1afea5b284
sarahrageh <sarargh2002@gmail.com>
2025-08-14 14:22:33 +0300

    [1789] [Time Beats: 27.96%] - LeetPush

---
+++ Databases/Primary-Department-for-Each-Employee.sql
@@ -0,0 +1,3 @@
+(select employee_id, department_id from employee where primary_flag = 'Y')
+union
+(select employee_id, department_id from employee group by employee_id having count(department_id) = 1);
