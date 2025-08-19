a2d1a3252556b1269774e8a9f5984bb2ea3b25af
sarahrageh <sarargh2002@gmail.com>
2025-08-19 16:26:29 +0300

    [602] [Time Beats: 43.32%] - LeetPush

---
+++ Databases/Friend-Requests-II:-Who-Has-the-Most-Friends.sql
@@ -0,0 +1,7 @@
+with cte as(
+    select requester_id as id from RequestAccepted
+    union all
+    select accepter_id from RequestAccepted
+)
+
+select id, count(*) as num from cte group by id order by count(*) desc limit 1;
